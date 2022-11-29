1a08aaec43c43ccdbc9df77f6ba64afea70c546d
FKRIK <fernandokrik@hotmail.com>
2022-11-29 20:41:13 -0300

    completo funcionario

--- src/app/pages/funcionarios/funcionarios.component.ts
+++ src/app/pages/funcionarios/funcionarios.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { Funcionario } from 'src/app/models/funcionario';
 import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { NonNullableFormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-funcionarios',
@@ -10,7 +13,13 @@ import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
 })
 export class FuncionariosComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+		private router: Router 
+  ) {}
+
+  funcionarios!: Funcionario[];
+  id?: number;
   nome?: string
   cpf?: string
 
@@ -19,10 +28,23 @@ export class FuncionariosComponent implements OnInit {
 	faPen = faPen;
 
   ngOnInit(): void {
+    this.http.get<Funcionario[]>("https://localhost:5001/api/funcionarios/listar").subscribe({
+			next: (funcionarios) => {
+				this.funcionarios = funcionarios;
+			},
+		});
   }
 
   cadastrar(): void{
+    let funcionario: Funcionario = {
+      id: this.id,
+      nome: this.nome,
+      cpf: this.cpf
+    };
 
+    this.http
+			.post<Funcionario>("https://localhost:5001/api/funcionarios/cadastrar", funcionario)
+			.subscribe({ next: (funcionario) => this.funcionarios.push(funcionario) });
   }
 
   // Passar o parãmetro ID e mudar no HTML também
