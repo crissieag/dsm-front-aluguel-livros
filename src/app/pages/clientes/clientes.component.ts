f2cb305de0121b18550d3374547ddcc63b74c5d8
FKRIK <fernandokrik@hotmail.com>
2022-11-29 20:27:15 -0300

    completo cliente

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { Cliente } from 'src/app/models/cliente';
 import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
@@ -9,8 +11,13 @@ import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+  constructor(
+    private http: HttpClient,
+		private router: Router 
+  ) {}
 
-  constructor() { }
+  clientes!: Cliente[];
+  id?: number;
   nome?: string
   cpf?: string
 
@@ -19,13 +26,30 @@ export class ClientesComponent implements OnInit {
 	faPen = faPen;
 
   ngOnInit(): void {
+    this.http.get<Cliente[]>("https://localhost:5001/api/clientes/listar").subscribe({
+			next: (clientes) => {
+				this.clientes = clientes;
+			},
+		});
   }
+
+  
 
   cadastrar(): void{
 
+    // montando o json
+    let cliente: Cliente = {
+      id: this.id,
+      nome: this.nome,
+      cpf: this.cpf
+    };
+
+    this.http
+			.post<Cliente>("https://localhost:5001/api/clientes/cadastrar", cliente)
+			.subscribe({ next: (cliente) => this.clientes.push(cliente) });
   }
 
-  // Passar o parãmetro ID e mudar no HTML também
+  // Passar o parâmetro ID e mudar no HTML também
   editarCliente(): void {}
 
 	excluirCliente(): void {}
